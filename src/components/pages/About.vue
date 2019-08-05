a9c1c5848aa2e624834a8139f01776971b445cd5
xch752 <1628265481@qq.com>
2019-08-05 15:27:24 +0800

    update

--- src/components/pages/About.vue
+++ src/components/pages/About.vue
@@ -5,6 +5,7 @@
             <h1 style="font-size:5vw">xch752</h1>
             <h2 style="font-size:3vw">1628265481@qq.com</h2>
             <h2 style="font-size:3vw"><span>GitHub:</span>https://github.com/xch752</h2>
+            <h2 style="font-size:3vw"><span>GitHub:</span>https://github.com/xch752/vue_management_jsonserver</h2>
         </div>
     </div>
 </template>
